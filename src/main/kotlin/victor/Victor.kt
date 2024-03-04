ee5324eeb1af3be600d4e5a631ce8a1dba45f612
ignaciogarcia <ig@pep.security>
2024-03-04 13:47:24 +0100

    Firt complete version but still much is missing

--- src/main/kotlin/victor/Victor.kt
+++ src/main/kotlin/victor/Victor.kt
@@ -14,6 +14,7 @@ class Victor {
     private val publicKey = PublicKey("VictorPublicKey")
     private val privateKey = PrivateKey("VictorPrivateKey")
     private val sessions = mutableListOf<VictorSession>()
+    private lateinit var peggyPublicKey: PublicKey
 
 
     fun stepOne(): StepOneMessage {
@@ -34,6 +35,79 @@ class Victor {
         return StepOneMessage(sessionId, publicKey, blindedVerifiers, signatureStepOne)
     }
 
+    fun stepThree(stepTwoMessage: StepTwoMessage): StepThreeMessage {
+        pgpVerify(stepTwoMessage.signature, stepTwoMessage.publicKey, stepTwoMessage.reBlindedVerifierPkHashes)
+        peggyPublicKey = stepTwoMessage.publicKey
+        val session = sessions.first { it.sessionId == stepTwoMessage.sessionId }
+        val k = stepTwoMessage.blindedSignatureHashes.size
+        val challenge = session.challenge.toMutableList()
+        if (challenge.isEmpty()) {
+            repeat(k) {
+                val randomBit = Random.nextInt(2)
+                challenge.add(randomBit)
+            }
+        }
+        val sessionId = session.sessionId
+        val signatureStepThree = pgpSign("$sessionId@#~${Json.encodeToString(challenge)}", privateKey)
+        val newSession = VictorSession(
+            sessionId = sessionId,
+            blindFactor = session.blindFactor,
+            blindedVerifiers = session.blindedVerifiers,
+            publicKey = publicKey, // here in reality we would not save the public key according to the thesis.
+            challenge = challenge,
+            blindedSignatureHashes = stepTwoMessage.blindedSignatureHashes,
+            reBlindedVerifierPkHashes = stepTwoMessage.reBlindedVerifierPkHashes,
+            message = message
+        )
+        store(newSession)
+        return StepThreeMessage(
+            sessionId,
+            challenge,
+            signatureStepThree
+        )
+    }
+
+    fun stepFive(stepFourMessage: StepFourMessage): Set<String> {
+        pgpVerify(stepFourMessage.signature, peggyPublicKey, stepFourMessage.response)
+        val sessionId = stepFourMessage.sessionId
+        val session = sessions.first { it.sessionId == sessionId }
+        val challenge = session.challenge
+        val response = stepFourMessage.response
+        val peggyReblindedVerifiers = session.reBlindedVerifierPkHashes
+        val peggyBlindedSignatureHashes = session.blindedSignatureHashes
+        assert(challenge.isNotEmpty())
+        var trust: Set<String> = setOf("-1")
+        val victorBlindFactor = session.blindFactor
+        val k = challenge.size
+        repeat(k) { i ->
+            if (challenge[i] == 0) {
+                val peggyBlindingFactor = response[i] as Double
+                val reblindedVerifierPkHashes = mutableSetOf<String>()
+                trustedVerifiers.forEach { verifierKey ->
+                    reblindedVerifierPkHashes.add(hashFunction(blindPublicKey(verifierKey, peggyBlindingFactor)))
+                }
+                assert(reblindedVerifierPkHashes == peggyReblindedVerifiers[i])
+            } else {
+                val (peggyBlindedAttesters, salt) = response[i] as BlindedAttestersAndSalt
+                val reblindedAttesterHashes = mutableSetOf<String>()
+                val blindedAttesterHashes = mutableSetOf<String>()
+                peggyBlindedAttesters.forEach { attester ->
+                    val (_, publicKey) = attester
+                    // here verify(signature, publicKey, message) but this is not PGP verify, so our own verification
+                    blindedAttesterHashes.add(hashFunction("${Json.encodeToString(attester)}@#~$salt"))
+                    reblindedAttesterHashes.add(hashFunction(blindPublicKey(publicKey, victorBlindFactor)))
+                }
+                assert(blindedAttesterHashes == peggyBlindedSignatureHashes)
+                if (trust == setOf("-1")) {
+                    trust = reblindedAttesterHashes.intersect(peggyReblindedVerifiers[i]) // assign trust
+                } else {
+                    assert(trust == reblindedAttesterHashes.intersect(peggyReblindedVerifiers[i])) // verify its same
+                }
+            }
+        }
+        return trust
+    }
+
     private fun store(session: VictorSession) {
         sessions.add(session)
     }
@@ -41,10 +115,20 @@ class Victor {
 
 fun blindFactGen(): Double = Random.nextDouble()
 
+fun hashFunction(what: Any): String {
+    return what.hashCode().toString()
+}
+
 data class StepOneMessage(
     val sessionId: String,
     val publicKey: PublicKey,
     val blindedVerifiers: List<PublicKey>,
+    val signature: Signature,
+)
+
+data class StepThreeMessage(
+    val sessionId: String,
+    val challenge: List<Int>,
     val signature: Signature,
 )
 
@@ -80,6 +164,10 @@ data class VictorSession(
     val blindFactor: Double,
     val blindedVerifiers: List<PublicKey>,
     val publicKey: PublicKey,
+    val challenge: List<Int> = emptyList(),
+    val blindedSignatureHashes: List<Set<String>> = emptyList(),
+    val reBlindedVerifierPkHashes: List<Set<String>> = emptyList(),
+    val message: Message? = null,
 )
 
 @Serializable
@@ -107,10 +195,12 @@ class Peggy {
     private val publicKey = PublicKey("PeggyPublicKey")
     private val privateKey = PrivateKey("PeggyPrivateKey")
     private val sessions = mutableListOf<PeggySession>()
+    private lateinit var victorPublicKey: PublicKey
+    private val k = 3
 
-    fun stepTwo(stepOneMessage: StepOneMessage, k: Int): StepTwoMessage {
+    fun stepTwo(stepOneMessage: StepOneMessage, k: Int = this.k): StepTwoMessage {
         val sessionId = stepOneMessage.sessionId
-        val victorPublicKey = stepOneMessage.publicKey
+        victorPublicKey = stepOneMessage.publicKey
         val signatureStepOne = stepOneMessage.signature
         pgpVerify(signatureStepOne, victorPublicKey, stepOneMessage.blindedVerifiers)
         val blindedVerifiers = stepOneMessage.blindedVerifiers
@@ -166,10 +256,40 @@ class Peggy {
         )
     }
 
+    fun stepFour(stepThreeMessage: StepThreeMessage): StepFourMessage {
+        pgpVerify(stepThreeMessage.signature, victorPublicKey, stepThreeMessage.challenge)
+        val session = sessions.first { it.sessionId == stepThreeMessage.sessionId }
+        val sessionId = session.sessionId
+        val blindingFactors = session.blindingFactors
+        val blindedAttesters = session.blindedAttesters
+        val salt = session.salt
+
+        val response = mutableListOf<Any>()
+        repeat(k) { i ->
+            if (stepThreeMessage.challenge[i] == 0) {
+                response.add(session.blindingFactors)
+            } else {
+                response.add(BlindedAttestersAndSalt(blindedAttesters[i], salt[i]))
+            }
+        }
+        val signatureStepFour = pgpSign("$sessionId@#~${Json.encodeToString(response)}", privateKey)
+        return StepFourMessage(
+            sessionId = sessionId,
+            response = response,
+            signature = signatureStepFour
+        )
+    }
+
     private fun store(session: PeggySession) {
         sessions.add(session)
     }
 }
+
+@Serializable
+data class BlindedAttestersAndSalt(
+    val blindedAttesters: Set<Attester>,
+    val salt: Double,
+)
 
 data class StepTwoMessage(
     val sessionId: String,
@@ -177,6 +297,12 @@ data class StepTwoMessage(
     val blindedSignatureHashes: List<Set<String>>,
     val reBlindedVerifierPkHashes: List<Set<String>>,
     val message: Message,
+    val signature: Signature,
+)
+
+data class StepFourMessage(
+    val sessionId: String,
+    val response: List<Any>,
     val signature: Signature,
 )
 
@@ -197,9 +323,13 @@ class FotDemo {
     private val messages = mutableListOf<Any>()
     private val k = 3
 
-    fun fot() {
+    fun fot(): Set<String> {
         stepOne()
         stepTwo(messages[0] as StepOneMessage)
+        stepThree(messages[1] as StepTwoMessage)
+        stepFour(messages[2] as StepThreeMessage)
+        val trust = stepFive(messages[3] as StepFourMessage)
+        return trust
     }
 
 
@@ -213,6 +343,20 @@ class FotDemo {
     fun stepTwo(stepOneMessage: StepOneMessage) {
         val stepTwoMessage = peggy.stepTwo(stepOneMessage, k)
         messages.add(stepTwoMessage)
+    }
+
+    fun stepThree(stepTwoMessage: StepTwoMessage) {
+        val stepThreeMessage = victor.stepThree(stepTwoMessage)
+        messages.add(stepThreeMessage)
+    }
+
+    fun stepFour(stepThreeMessage: StepThreeMessage) {
+        val stepFourMessage = peggy.stepFour(stepThreeMessage)
+        messages.add(stepFourMessage)
+    }
+
+    fun stepFive(stepFourMessage: StepFourMessage): Set<String> {
+        return victor.stepFive(stepFourMessage)
     }
 }
 
